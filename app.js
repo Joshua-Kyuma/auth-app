eb5f54ea91a0c350e95b6dd6c5905fb88f9c212c
joshua_mwongela <joshua.mwongela001@gmail.com>
2023-03-01 09:36:58 +0300

    created register and login paths and database models in Mongo

--- app.js
+++ app.js
@@ -2,12 +2,66 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req,res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save().then(function(){
+    res.render("secrets");
+  }).catch(function(){
+    console.log(err);
+  });
+});
+
+app.post("/login", function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}) .then(function(foundUser){
+
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    })
+    .catch(function(err){
+      res.send(400, "Bad request");
+    });
+});
+
+
 
 
 
